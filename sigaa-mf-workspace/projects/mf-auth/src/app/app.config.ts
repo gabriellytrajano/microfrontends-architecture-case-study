48d4b34f9e3f33bf6664a6377406fe90461090a4
Gabrielly <gabrielly.amorin@ccc.ufcg.edu.br>
2026-02-01 00:20:07 -0300

    fix

--- sigaa-mf-workspace/projects/mf-auth/src/app/app.config.ts
+++ sigaa-mf-workspace/projects/mf-auth/src/app/app.config.ts
@@ -1,8 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { ROUTES } from './app.routes';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(ROUTES)],
+  providers: [provideRouter(routes)],
 };
